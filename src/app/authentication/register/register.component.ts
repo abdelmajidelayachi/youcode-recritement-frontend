b310d36a770b48d740e3edecb8c0c1927bae69ad
EL Ayachi Abdelmajid <93975585+EL-Ayachi-Abdelmajid@users.noreply.github.com>
2023-01-27 16:47:35 +0100

    add register

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import {map} from "rxjs/operators"
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
-
+  registerForm: FormGroup;
+  constructor(private authService: AuthService, private router : Router) { 
+   this.registerForm = new FormGroup({
+    firstName: new FormControl(null,   [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+    lastName: new FormControl(null,   [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+    email: new FormControl(null,   [Validators.required,Validators.email]),
+   password: new FormControl(null,[Validators.required, Validators.minLength(6), Validators.maxLength(20)])
+   });
+   }
   ngOnInit(): void {
-  }
+   }
+   
+   get firstName(){
+    return this.registerForm.get('firstName');
+    }
+    get lastName(){
+    return this.registerForm.get('lastName');
+    }
+
+   get email(){
+    return this.registerForm.get('email');
+   }
+    get password(){
+    return this.registerForm.get('password');
+    }
+
+    submitLogin(){
+      this.authService.registerAuth(this.registerForm.value).subscribe(
+        (data) => {
+          this.router.navigate(['/auth/login']);
+        },
+        (error) => {
+          alert(error.error.errors[0]);
+        }
+      );
+      
+    }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Constants } from '../shared/Constants';
@@ -14,12 +14,18 @@ export class AuthService {
     return this.http.post(Constants.apiEndPoint.auth.login, {email, password});
   }
 
+  registerAuth(data:any){
+    data.role = 'candidate';
+    return this.http.post(Constants.apiEndPoint.auth.register, data);
+  }
+
   setCredentials(token: any){
     if(token){
       localStorage.setItem('access_token', token);
       this.route.navigate(['/']);
     }
   }
+  
 
   getCredentials(){
     return localStorage.getItem('access_token');
