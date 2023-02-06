a0acf3cd31ca44327779c854024a3f3533b034cc
mohammedaminelahmami <lahmamimohammedamine@gmail.com>
2023-02-06 14:38:38 +0100

    add home-candidate

--- src/app/pages/dashboard/candidates/candidates.component.ts
+++ src/app/pages/dashboard/candidates/candidates.component.ts
@@ -5,47 +5,44 @@ import { User } from 'src/app/shared/interfaces/User';
 @Component({
   selector: 'app-candidates',
   templateUrl: './candidates.component.html',
-  styleUrls: ['./candidates.component.css']
+  styleUrls: ['./candidates.component.css'],
 })
 export class CandidatesComponent {
   pagination = {
     currentPage: 1,
     itemsPerPage: 10,
-    totalItems: 0
-  }
+    totalItems: 0,
+  };
 
-  Candidates : User[] = [];
-
+  Candidates: User[] = [];
 
   // pagination
   handlePageChanged(event: any) {
     this.pagination.currentPage = event;
-    this.CandidateService.getCandidates(event).subscribe((res:any) => {
+    this.CandidateService.getCandidates(event).subscribe((res: any) => {
       this.Candidates = res.data;
       this.pagination.totalItems = res.lastPage;
       this.pagination.itemsPerPage = 10;
       this.pagination.currentPage = res.pageNumber;
-    })
+    });
   }
 
-  constructor(private CandidateService: CandidateService) { }
+  constructor(private CandidateService: CandidateService) {}
   ngOnInit(): void {
-    this.CandidateService.getCandidates().subscribe((res:any) => {
+    this.CandidateService.getCandidates().subscribe((res: any) => {
       console.log(res);
       this.Candidates = res.data;
       this.pagination.totalItems = res.lastPage;
       this.pagination.itemsPerPage = 10;
       this.pagination.currentPage = res.pageNumber;
-    })    
+    });
   }
 
-  searchText: string='';
+  searchText: string = '';
 
   // create method call custom method
-  onSerachTextEntered(searchValue: string){
-    this.searchText=searchValue;
+  onSerachTextEntered(searchValue: string) {
+    this.searchText = searchValue;
     // console.log(this.searchText);
   }
-    
-
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,3 +1,5 @@
+import { SubSkillsComponent } from './../../components/skills/sub-skills/sub-skills.component';
+import { SkillsBarComponent } from './../../components/skills/skills-bar/skills-bar.component';
 import { CoreModule } from './../../core/core.module';
 import { CommentComponent } from './../../components/candidate/comment/comment.component';
 import { MainNavComponent } from './../../components/candidate/main-nav/main-nav.component';
@@ -16,12 +18,16 @@ import { LetterMotivationComponent } from './letter-motivation/letter-motivation
     MainNavComponent,
     CommentComponent,
     LetterMotivationComponent,
+    SkillsBarComponent,
+    SubSkillsComponent,
   ],
   exports: [
     ProfileComponent,
     CandidateHomeComponent,
     MainNavComponent,
     CommentComponent,
+    SkillsBarComponent,
+    SubSkillsComponent,
   ],
   imports: [
     CommonModule,
